aa585521f34f3fc8e79c50346454206e6009d2c3
Masato Miyamoto <project-europa@excite.co.jp>
2020-01-27 09:28:30 +0900

    delete: 不要なcss styleを削除

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-parallax dark :src="require('../../assets/images/Europa.jpg')" style="height: 100%;transform: initial;">
+  <v-parallax dark :src="require('../../assets/images/Europa.jpg')" style="height: 100%;">
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
         <h1 class="display-1 font-weight-bold mb-4 cyan--text text--darken-1">Welcome to Europa</h1>
